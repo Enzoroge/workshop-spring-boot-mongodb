fce8db3487368b30088305b0146c3271d97b4e8c
Enzoroge <rogeriolima20042000@yahoo.com.br>
2022-12-07 20:23:38 -0300

    Inserção de usuário com Post

--- src/main/java/com/enzoroge/workshopmongo/service/UserService.java
+++ src/main/java/com/enzoroge/workshopmongo/service/UserService.java
@@ -13,20 +13,26 @@ import com.enzoroge.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	
-	
-	public List<User> findAll(){
-			return repository.findAll();
-		
-	}
-	
-	public User findById(String id) {
-		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+
+	public List<User> findAll() {
+		return repository.findAll();
+
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+
+	}
 }
