9b8cf39c909dfc289235155d3b2803de9a641918
Enzoroge <rogeriolima20042000@yahoo.com.br>
2022-12-13 14:17:56 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/enzoroge/workshopmongo/resource/UserResource.java
+++ src/main/java/com/enzoroge/workshopmongo/resource/UserResource.java
@@ -10,56 +10,63 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.enzoroge.workshopmongo.domain.Post;
 import com.enzoroge.workshopmongo.domain.User;
 import com.enzoroge.workshopmongo.dto.UserDto;
 import com.enzoroge.workshopmongo.service.UserService;
 
 @RestController
-@RequestMapping(value= "/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<UserDto>> findAll(){
-		List<User>list = service.findAll();
-		List<UserDto> listDto = list.stream().map (x -> new UserDto(x)).collect(Collectors.toList());
+	public ResponseEntity<List<UserDto>> findAll() {
+		List<User> list = service.findAll();
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value = "/{id}",method=RequestMethod.GET)
-	public ResponseEntity<UserDto> findById(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDto> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDto(obj));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody UserDto objDto){
+	public ResponseEntity<Void> insert(@RequestBody UserDto objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objDto.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-}
+	}
+
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable String id){
+	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-}
-	@RequestMapping(value = "/{id}",method=RequestMethod.PUT)
-	public ResponseEntity<Void> update (@RequestBody UserDto objDto,  @PathVariable String id){
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDto objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
-	
-		
+
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
