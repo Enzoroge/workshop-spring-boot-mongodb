da84a2271993f91544b5796b6938d032832127ca
Enzoroge <rogeriolima20042000@yahoo.com.br>
2022-12-14 11:28:02 -0300

    Consulta simples com @query

--- src/main/java/com/enzoroge/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/enzoroge/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.enzoroge.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.enzoroge.workshopmongo.domain.Post;
@@ -11,6 +12,10 @@ import com.enzoroge.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	//ignoreCase faz a busca ignorar letras maiuscula ou minuscula
+	
+	@Query("{'title' :{$regex: ?0, $options : 'i'}}")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findBytitleContainingIgnoreCase(String text);
 
 }
